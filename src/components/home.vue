dc12383025a9a5e538343d5f233d35550fa580a6
FilaMarek <48298031+FilaMarek@users.noreply.github.com>
2020-11-11 19:46:39 +0100

    Building Menu
    So far i have issue with Menu i may created it my self without bootstrap

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="main">
-
     <div class="toraWearPic">
       <div class="logo">
-        <img src="../assets/logo.jpg" alt="Tora Shop Logo">
+       <!-- <img src="../assets/logo.jpg" alt="Tora Shop Logo"> -->
       </div>
     </div>
 
@@ -38,7 +37,7 @@ export default {
       About: "About Us",
 
     }
-  }
+  },
 }
 </script>
 
@@ -48,71 +47,54 @@ export default {
   .toraWearPic{
     width: 100%;
     height:900px ;
-    background-color:black;
     border: 1px solid black;
 
-  }
+  background-position: center;
+  display: grid;
+  grid-template-columns: 50% 50%;
 
-  .logo{
-    display: block;
-    height: 100%;
-    margin-left: auto;
-    margin-right: auto;
-    width: 50%;
   }
-
 
 .secondLine{
     width: 100%;
-    height:400px ;
+    height:400px;
     border: 1px solid black;
-    display: flex;
+  background-size: cover;
+  background-position: center;
+  display: grid;
+  grid-template-columns: 50% 50%;
 }
 
    .leftPicTora{
     border: 1px solid black;
-    flex-direction: column;
-    height:400px ;
-    width:50%;
    }
   .rightPicTora{
     border: 1px solid black;
-    flex: 1;
-    flex-direction: column;
-    height:400px ;
-    width:50%;
+
   }
-
-
 
 .thirdLine
 {
 width: 100%;
 height:400px ;
 border: 1px solid black;
-display: flex;
-}
 
+background-size: cover;
+background-position: center;
+display: grid;
+grid-template-columns: 33.33% 33.33% 33.33%;
+}
 
 .Categories1{
   border: 1px solid black;
-  flex-direction: column;
-  height:400px ;
-  width:33.333333%;
 }
 
 .Categories2{
   border: 1px solid black;
-  flex-direction: column;
-  height:400px ;
-  width:33.333333%;
 }
 
 .Categories3{
   border: 1px solid black;
-  flex-direction: column;
-  height:400px ;
-   width:33.333333%;
 }
 
 .fourthLine{
@@ -120,30 +102,22 @@ display: flex;
     height:600px ;
     /*background-color:black; */
     border: 1px solid black;
-
 }
 
 .fifthLine{
-    width: 100%;
-    height:750px ;
-    border: 1px solid black;
-
- background-image: url("../assets/aboutUs.jpg");
-	background-size: cover;
-	background-position: center;
-
-
-display: grid;
-grid-template-columns: 50% 50%;
+  width: 100%;
+  height:750px ;
+  border: 1px solid black;
+  background-image: url("../assets/aboutUs.jpg");
+  background-size: cover;
+  background-position: center;
+  display: grid;
+  grid-template-columns: 50% 50%;
 }
 
 .aboutUs{
 background: rgba(0, 0, 0, 0.726);
   color: white;
 }
-
-
-
-
 
 </style>
